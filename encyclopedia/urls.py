20f0b847f40936885a855a635ab33a39c8b4d479
Liliia Smelova <liliia.smelova@outlook.com>
2020-07-02 18:40:45 +0300

    implement entry page and search

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:name>", views.entry, name="entry"),
+    path("search_entry", views.search_entry, name="search_entry"),
+    path("new_page", views.new_page, name="new_page"),
+    path("random", views.random, name="random")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
+import markdown2
+import re
 
 from . import util
 
@@ -8,3 +11,48 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, name):
+        entry = util.get_entry(name)
+        if not entry:
+            return HttpResponseNotFound('<h1>Page not found</h1>')
+        entry = markdown2.markdown(entry)
+        return render(request, "encyclopedia/entry.html", {
+        "name": name,
+        "entry": entry
+    })
+
+
+
+def search_entry (request):
+    title = request.POST.get('q')
+    entry = util.get_entry(title)
+    list_titles =  util.list_entries()
+
+    print(request.POST.get('q'))
+    if entry:
+        entry = markdown2.markdown(entry)
+        return  render(request, "encyclopedia/entry.html", {
+        "name": title,
+        "entry": entry
+        })
+    result = []
+    for item in list_titles:
+        if re.search(title, item, re.IGNORECASE):
+            result.append(item)
+
+    return  render(request, "encyclopedia/search_results.html", {
+        "name": title,
+        "entries": result
+        })
+
+
+
+def new_page(request):    
+        return render(request, "encyclopedia/new_page.html", {
+        # "entries": util.list_entries()
+    })
+
+def random(request):
+    return render(request, "encyclopedia/random.html", {
+    # "entries": util.list_entries()
+})
