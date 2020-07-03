acb28a0f0791628c7225affa54dd4a6c28e8b48a
Liliia Smelova <liliia.smelova@outlook.com>
2020-07-03 19:19:04 +0300

    rewrite entry creation form

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django import forms
 from django.http import HttpResponse, HttpResponseNotFound
 from django.contrib import messages
 from django.shortcuts import redirect
@@ -6,21 +7,32 @@ import markdown2
 import re
 import random
 from django.core.files.storage import default_storage
+from django.core.validators import RegexValidator
 
 from . import util
 
 
+class CreatePageForm(forms.Form):
+    title = forms.CharField(label="Enter a title", max_length=30,
+                            widget=forms.TextInput(
+                                attrs={'class': 'form-control mb-4'}),
+                            validators=[RegexValidator('^[a-zA-Z0-9 -]*$', message="Title must be between 1 to 30 characters, contain only Latin letters, digits, dashes and spaces")])
+    description = forms.CharField(widget=forms.Textarea(
+        attrs={'class': 'form-control', "rows": "17"}), label="Enter a description", max_length=1000)
+
+
 def index(request):
     """
-    render list of all enries
+    Renders list of all enries
     """
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
-def entry(request, name):    
+
+def entry(request, name):
     """
-    render HTML entry page 
+    Renders HTML entry page 
     """
     entry = util.get_entry(name)
     if not entry:
@@ -28,57 +40,72 @@ def entry(request, name):
     # convert markdown into HTML
     entry = markdown2.markdown(entry)
     return render(request, "encyclopedia/entry.html", {
-    "name": name,
-    "entry": entry
+        "name": name,
+        "entry": entry
     })
 
 
-
-def search_entry (request):
+def search_entry(request):
+    """
+    Search by title. Redirects to a search results page or, if a complete match, to 
+    an entry page 
+    """
     title = request.POST.get('q')
     entry = util.get_entry(title)
-    list_titles =  util.list_entries()
+    list_titles = util.list_entries()
 
     if entry:
+        # convert markdown into HTML
         entry = markdown2.markdown(entry)
-        return  redirect("entry", title)
-        
+        return redirect("entry", title)
+
     result = []
     for item in list_titles:
         if re.search(title, item, re.IGNORECASE):
             result.append(item)
 
-    return  render(request, "encyclopedia/search_results.html", {
+    return render(request, "encyclopedia/search_results.html", {
         "name": title,
         "entries": result
+    })
+
+
+def new_page(request):
+    """
+    Renders page with form for creation new page
+    """
+    if request.method == "GET":
+        return render(request, "encyclopedia/new_page.html", {
+            "form": CreatePageForm()
         })
-
-
-
-def new_page(request): 
-    if request.method == "GET":   
-        return render(request, "encyclopedia/new_page.html")
     if request.method == "POST":
-        pattern = r'^[a-z0-9 -]{1,30}$'
-        title = request.POST.get('title')
-        content = request.POST.get("description")
-        # check if form data is valid
-        if not re.match(pattern, title, re.IGNORECASE):
-            messages.add_message(request, messages.ERROR, 'Title must be between 1 to 30 characters, contain only Latin letters, digits, dashes and spaces')
-            return render(request, "encyclopedia/new_page.html")
+        form = CreatePageForm(request.POST)
+        if not form.is_valid():
+            return render(request, "encyclopedia/new_page.html", {
+                "form": form
+            })
+        title = form.cleaned_data["title"]
+        content = form.cleaned_data["description"]
+
         filename = f"entries/{title}.md"
         if default_storage.exists(filename):
-            messages.add_message(request, messages.ERROR, 'Entry already exists with the provided title!')
-            return render(request, "encyclopedia/new_page.html")
+            messages.add_message(request, messages.ERROR,
+                                 'Entry already exists with the provided title!')
+            return render(request, "encyclopedia/new_page.html", {
+                "form": form
+            })
         else:
             util.save_entry(title, content)
             return redirect("entry", title)
-        
 
-def edit_page (request, title):
-    if request.method == "GET":   
+
+def edit_page(request, title):
+    """
+    Renders page with forms for editing existing entries
+    """
+    if request.method == "GET":
         content = util.get_entry(title)
-        return render(request, "encyclopedia/edit.html", {"title":title, "content" : content})
+        return render(request, "encyclopedia/edit.html", {"title": title, "content": content})
 
     if request.method == "POST":
         content = request.POST.get("description")
@@ -88,7 +115,7 @@ def edit_page (request, title):
 
 def random_page(request):
     """
-    redirect to a rendom page
+    Redirects to a random page
     """
     list_entries = util.list_entries()
     title = random.choice(list_entries)
